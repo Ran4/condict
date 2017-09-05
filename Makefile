2656ac0b48ee151d0aa91428d41e5e60d9452d0b
Rasmus Ansin <rasmus.ansin@orexplore.com>
2017-09-05 15:07:15 +0200

    Add LICENSE, MANIFEST.in, setup.py

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-.PHONY: test
+.PHONY: test dist clean
+dist:
+	python3 setup.py sdist
+clean:
+	rm -rf MANIFEST dist
 test:
 	@python3 -m unittest
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='conDict',
+    version='0.1',
+    description="Python library containing functions that helps with conditionally creating dicts.",
+    author="Rasmus Ansin",
+    author_email="rasmus.ansin@gmail.com",
+    url="https://github.com/Ran4/condict",
+    packages=['condict',],
+    license="MIT",
+    long_description=open('README.md').read(),
+)
